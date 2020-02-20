eaddc5f97da06e5f273bb47ce8513e0762ff6a90
Milo Rastgoo <hi@informagician.com>
2020-02-19 19:04:29 -0500

    add step partially

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,10 +3,11 @@ const db = require('../data/db-config.js');
 module.exports = {
     find,
     findById,
-    // findSteps,
-    // add,
-    // update,
-    // remove
+    findSteps,
+    add,
+    update,
+    remove,
+    addStep
 }
 
 function find() {
@@ -17,4 +18,32 @@ function findById(id) {
     return db('schemes')
     .where({id})
     .first()
+}
+
+function findSteps(id) {
+    return db('schemes')
+    .join('steps', 'steps.scheme_id', 'schemes.id')
+    .where({scheme_id:id})
+}
+
+function add(scheme) {
+    return db('schemes')
+    .insert(scheme, 'id')
+}
+
+function update(changes, id) {
+    return db('schemes')
+    .where({id})
+    .update(changes)
+}
+
+function remove(id) {
+    return db('schemes')
+    .where({id})
+    .del()
+}
+
+function addStep(step, id){
+    return db('schemes')
+    
 }
